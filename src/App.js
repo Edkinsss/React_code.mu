e30f3ab44aa4e710990aae7c9fe825c162f296ac
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-03-05 22:44:01 +0300

    Введение в язык JSX в React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
-	return <div>
-		text
-	</div>;
+	return <div class="aaa">
+    <h1 class="bbb">
+      First
+    </h1>
+    <h2 class="ccc">
+      Second
+    </h2>
+  </div>
 }
 
 export default App;
