71ad72d8766687c9b0090c5b3d7a29bc9c30cd6b
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-03-06 08:31:23 +0300

    Вставка значений переменных и констант в JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <input />, 
-      <input />, 
-      <input />, 
-    </div>
-  )
+	const str1 = 'text1';
+	const str2 = 'text1';
+	
+	return <div>
+		<p>{str1}</p>
+		<p>{str2}</p>
+	</div>;
 }
 
 export default App;
