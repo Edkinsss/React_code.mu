1b42eba1eb1e04943ecfae8811a452242d835aae
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2025-03-06 09:15:09 +0300

    Теги на разных строках в JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function App() {
-    const str = <> <ul> <li>1</li> <li>2</li> <li>3</li> </ul> </>;
-
-  return <main>
-    {str}
-  </main>;
+	const items = (
+    <ul>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
+    </ul>
+  )
+	
+	return <ul>
+		{items}
+	</ul>
 }
 
 
